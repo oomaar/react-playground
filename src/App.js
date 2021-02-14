cd59b9b399ccf94853448622753dca38021c6dc9
Omar <omaar_5@hotmail.com>
2021-02-14 16:42:52 +0200

    hooks review

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => setToggle(state => !state);
+
+  useEffect(() => {
+    const everyRender = "I Will Run On Every Render";
+    console.log("🚀 ~ file: App.js ~ line 11 ~ useEffect ~ everyRender", everyRender);
+  });
+
+  useEffect(() => {
+    const firtRender = "I Will Run On First Render";
+    console.log("🚀 ~ file: App.js ~ line 16 ~ useEffect ~ firtRender", firtRender);
+  }, []);
+
+  useEffect(() => {
+    const depend = "I Will Run On First Render and Every Time State Changes"; 
+    console.log("🚀 ~ file: App.js ~ line 21 ~ useEffect ~ depend", depend);
+  }, [toggle]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="text__container">
+        <h1>Title Lands Here</h1>
+        {toggle && (
+          <p>Text Lands Here</p>
+        )}
+        <button onClick={handleToggle}>Click Me</button>
+      </div>
     </div>
   );
 }
